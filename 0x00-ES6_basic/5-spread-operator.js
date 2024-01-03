f6b536350701660c42c7be55955267c81901da3a
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-02 16:11:07 -0800

    updated budget manipulation

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+export default function concatArrays(array1, array2, string) {
+    const srt = [...array1,...array2,...string]
+    return srt
+}
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
