fc0bf3734aaaa5a723d56e6527357e6269dd1915
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-03 15:09:24 -0800

    creiterator function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,2 +1,7 @@
-#!/usr/bin/node
-
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const item of reportWithIterator) {
+    employees.push(item);
+  }
+  return employees.join(' | ');
+}
