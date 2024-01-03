982c0372ee554471650a1be96cc8e193807bdb0a
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-03 08:29:15 -0800

    return parameter function

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+export default function createIteratorObject(report) {
+    return {
+      *[Symbol.iterator]() {
+        for (const department in report.allEmployees) {
+          const employees = report.allEmployees[department];
+          for (const employee of employees) {
+            yield employee;
+          }
+        }
+      }
+    };
+  }
+  
