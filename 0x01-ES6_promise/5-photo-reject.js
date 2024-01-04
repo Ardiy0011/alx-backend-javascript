6464dba5f45857a7510d5877407d91f3185e7802
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-04 11:01:47 -0800

    creiterator function

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,6 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(
-    new Error(`${filename} cannot be processed`),
+  return new Promise((reject)=>{
+    reject(new Error(`${filename} cannot be processed`));
+  }
   );
 }
