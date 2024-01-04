f4fba77ed4c5f138a4c12b216ce7cc1b9d42efb3
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-03 16:54:52 -0800

    creiterator function

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    const idx = array.indexOf(value);
-    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
+    let newArray = [];
+    for (let value of array) {
+      newArray.push(appendString + value);
+    }
+  
+    return newArray;
   }
-
-  return array;
-}
